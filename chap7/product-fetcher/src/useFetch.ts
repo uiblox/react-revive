d4f59024a61adb3486654831b36af167f6b79b23
Christian <bournecreative@gmail.com>
2026-01-06 21:00:44 -0600

    Add loading and error state to useFetch hook

--- chap7/product-fetcher/src/useFetch.ts
+++ chap7/product-fetcher/src/useFetch.ts
@@ -3,22 +3,31 @@ import { type Book } from "./ProductTypes";
 
 export const useFetch = (url: string) => {
   const [data, setData] = useState<Book[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null | string>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const response = await fetch(url);
         if (!response.ok) {
+          setError(response.statusText);
           throw new Error(response.statusText);
         }
         const data = await response.json();
         setData(data);
-      } catch (error) {
-        console.log(error);
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          console.log(e.message);
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
   }, [url]);
 
-  return { data: data };
+  return { data: data, error: error, loading: loading };
 };
